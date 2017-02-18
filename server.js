67b6220ed94531d69746b50d1effdf2c85c4de61
Bhupinder <apple@Bhupinders-MacBook-Pro.local>
2017-02-18 21:42:38 +0530

    making ready for running on heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
